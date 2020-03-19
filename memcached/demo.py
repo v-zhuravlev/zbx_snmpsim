b0de848884ea4fb91bec91f91d8244f827079b7a
Yulia Chukina <yulia.chukina@zabbix.com>
2020-03-19 15:44:15 +0300

    add fill-in script and host

--- memcached/demo.py
+++ memcached/demo.py
@@ -1,23 +1,18 @@
-#!/Library/Frameworks/Python.framework/Versions/3.6/bin/python3.6
+#!/bin/env python3
 
 from pymemcache.client import base
 
 client1 = base.Client(('localhost', 11211))
 client2 = base.Client(('localhost', 11211))
 client3 = base.Client(('localhost', 11211))
-for i in range(1000):
-    client1.set('some_key'+str(i),str(i))
 
-for i in range(5000):
+for i in range(100,4565):
     client2.set('another_key'+str(i),str(i))
 
-for i in range(10000,145435):
-    client2.set('another_key'+str(i),str(i))
-
-for i in range(9999,34343):
+for i in range(9999,13433):
     client1.get('some_key'+str(i))
 
-for i in range(10000,345435):
+for i in range(10000,34535):
     client2.set('another_key'+str(i),str(i))
 
 for i in range(1000):
