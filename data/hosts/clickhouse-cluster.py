6577555a54502e490e7f3c75af1d973074c8e327
ATich <mail_me@atich.pro>
2020-03-29 14:13:02 +0300

    fix docker file, added hosts data

---
+++ data/hosts/clickhouse-cluster.py
@@ -0,0 +1,124 @@
+def create_host(zapi):
+
+    shard1_rep1 = {
+        "host": "clickhouse_1_1",
+        "interfaces": [
+            {
+                "type": 1,
+                "main": 1,
+                "useip": 0,
+                "ip": "",
+                "dns": "ch1",
+                "port": "10050"
+            }
+        ],
+        "groups": zapi.hostgroup.get(filter={"name": "Templates/Databases"}, output=['id']),
+        "tags": [
+            {
+                "tag": "Clickhouse docker test",
+                "value": ""
+            }
+        ],
+        "templates": [
+            zapi.template.get(filter={"name": "CH_man"}, output=['id'])[0]
+        ],
+        "inventory_mode": 0
+    }
+    shard1_rep2 = {
+        "host": "clickhouse_1_2",
+        "interfaces": [
+            {
+                "type": 1,
+                "main": 1,
+                "useip": 0,
+                "ip": "",
+                "dns": "ch2",
+                "port": "10050"
+            }
+        ],
+        "groups": zapi.hostgroup.get(filter={"name": "Templates/Databases"}, output=['id']),
+        "tags": [
+            {
+                "tag": "Clickhouse docker test",
+                "value": ""
+            }
+        ],
+        "templates": [
+            zapi.template.get(filter={"name": "CH_man"}, output=['id'])[0]
+        ],
+
+        "macros": [
+            {
+                "macro": "{$CH.PORT}",
+                "value": "8123"
+            }
+        ],
+        "inventory_mode": 0
+    }
+
+    shard2_rep1 = {
+        "host": "clickhouse_2_1",
+        "interfaces": [
+            {
+                "type": 1,
+                "main": 1,
+                "useip": 0,
+                "ip": "",
+                "dns": "ch3",
+                "port": "10050"
+            }
+        ],
+        "groups": zapi.hostgroup.get(filter={"name": "Templates/Databases"}, output=['id']),
+        "tags": [
+            {
+                "tag": "Clickhouse docker test",
+                "value": ""
+            }
+        ],
+        "templates": [
+            zapi.template.get(filter={"name": "CH_man"}, output=['id'])[0]
+        ],
+        "macros": [
+            {
+                "macro": "{$CH.PORT}",
+                "value": "8123"
+            }
+        ],
+        "inventory_mode": 0
+    }
+
+    shard2_rep2 = {
+        "host": "clickhouse_2_2",
+        "interfaces": [
+            {
+                "type": 1,
+                "main": 1,
+                "useip": 0,
+                "ip": "",
+                "dns": "ch4",
+                "port": "10050"
+            }
+        ],
+        "groups": zapi.hostgroup.get(filter={"name": "Templates/Databases"}, output=['id']),
+        "tags": [
+            {
+                "tag": "Clickhouse docker test",
+                "value": ""
+            }
+        ],
+        "templates": [
+            zapi.template.get(filter={"name": "CH_man"}, output=['id'])[0]
+        ],
+         "macros": [
+            {
+                "macro": "{$CH.PORT}",
+                "value": "8123"
+            }
+        ],
+        "inventory_mode": 0
+    }
+
+    zapi.host.create(shard1_rep1)
+    zapi.host.create(shard1_rep2)
+    zapi.host.create(shard2_rep1)
+    zapi.host.create(shard2_rep2)
