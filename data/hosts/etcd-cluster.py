110352d673c75b7b55cc46cd0290c2beb3055ee8
ATich <mail_me@atich.pro>
2020-04-29 18:35:40 +0300

    fix compose and host data

--- data/hosts/etcd-cluster.py
+++ data/hosts/etcd-cluster.py
@@ -1,7 +1,7 @@
 def create_host(zapi):
 
     etcd1 = {
-        "host": "etcd1",
+        "host": "etcs cluster",
         "interfaces": [
             {
                 "type": 1,
@@ -20,60 +20,10 @@ def create_host(zapi):
             }
         ],
         "templates": [
-            zapi.template.get(filter={"name": "Template App Etcd by HTTP"}, output=['id'])[0]
+            zapi.template.get(filter={"name": "Template App Etcd Cluster by HTTP"}, output=['id'])[0]
         ],
         "inventory_mode": 0
     }
 
-    etcd2 = {
-        "host": "etcd2",
-        "interfaces": [
-            {
-                "type": 1,
-                "main": 1,
-                "useip": 0,
-                "ip": "",
-                "dns": "etcd2",
-                "port": "10050"
-            }
-        ],
-        "groups": zapi.hostgroup.get(filter={"name": "Templates/Applications"}, output=['id']),
-        "tags": [
-            {
-                "tag": "etcd docker test",
-                "value": ""
-            }
-        ],
-        "templates": [
-            zapi.template.get(filter={"name": "Template App Etcd by HTTP"}, output=['id'])[0]
-        ],
-        "inventory_mode": 0
-    }
-
-    etcd3 = {
-        "host": "etcd3",
-        "interfaces": [
-            {
-                "type": 1,
-                "main": 1,
-                "useip": 0,
-                "ip": "",
-                "dns": "etcd3",
-                "port": "10050"
-            }
-        ],
-        "groups": zapi.hostgroup.get(filter={"name": "Templates/Applications"}, output=['id']),
-        "tags": [
-            {
-                "tag": "etcd docker test",
-                "value": ""
-            }
-        ],
-        "templates": [
-            zapi.template.get(filter={"name": "Template App Etcd by HTTP"}, output=['id'])[0]
-        ],
-        "inventory_mode": 0
-    }
+ 
     zapi.host.create(etcd1)
-    zapi.host.create(etcd2)
-    zapi.host.create(etcd3)
