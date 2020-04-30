880e52245654bd1acc8215809b39139ffc8a764c
ATich <mail_me@atich.pro>
2020-04-30 17:16:48 +0300

    add v3.2 cluster with API v2

--- data/hosts/etcd-cluster.py
+++ data/hosts/etcd-cluster.py
@@ -1,6 +1,6 @@
 def create_host(zapi):
 
-    etcd1 = {
+    etcd_v34 = {
         "host": "etcs cluster",
         "interfaces": [
             {
@@ -25,5 +25,33 @@ def create_host(zapi):
         "inventory_mode": 0
     }
 
+
+
+    etcd_v32 = {
+        "host": "etcs v3.2 cluster",
+        "interfaces": [
+            {
+                "type": 1,
+                "main": 1,
+                "useip": 0,
+                "ip": "",
+                "dns": "etcdv2_1",
+                "port": "10050"
+            }
+        ],
+        "groups": zapi.hostgroup.get(filter={"name": "Templates/Applications"}, output=['id']),
+        "tags": [
+            {
+                "tag": "etcd docker test",
+                "value": ""
+            }
+        ],
+        "templates": [
+            zapi.template.get(filter={"name": "Template App Etcd Cluster by HTTP"}, output=['id'])[0]
+        ],
+        "inventory_mode": 0
+    }
+
  
-    zapi.host.create(etcd1)
+    zapi.host.create(etcd_v32)
+    zapi.host.create(etcd_v34)
