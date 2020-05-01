2958403be5940e4d616584306252fa5b0eda98dd
ATich <mail_me@atich.pro>
2020-05-01 18:00:01 +0300

    return to single-node template

--- data/hosts/etcd-cluster.py
+++ data/hosts/etcd-cluster.py
@@ -1,7 +1,7 @@
 def create_host(zapi):
 
-    etcd_v34 = {
-        "host": "etcs cluster",
+    etcd_v34_1 = {
+        "host": "etcd_v34 node 1",
         "interfaces": [
             {
                 "type": 1,
@@ -25,17 +25,15 @@ def create_host(zapi):
         "inventory_mode": 0
     }
 
-
-
-    etcd_v32 = {
-        "host": "etcs v3.2 cluster",
+    etcd_v34_2 = {
+        "host": "etcd_v34 node 2",
         "interfaces": [
             {
                 "type": 1,
                 "main": 1,
                 "useip": 0,
                 "ip": "",
-                "dns": "etcdv2_1",
+                "dns": "etcd2",
                 "port": "10050"
             }
         ],
@@ -52,6 +50,111 @@ def create_host(zapi):
         "inventory_mode": 0
     }
 
- 
-    zapi.host.create(etcd_v32)
-    zapi.host.create(etcd_v34)
+    etcd_v34_3 = {
+        "host": "etcd_v34 node 3",
+        "interfaces": [
+            {
+                "type": 1,
+                "main": 1,
+                "useip": 0,
+                "ip": "",
+                "dns": "etcd2",
+                "port": "10050"
+            }
+        ],
+        "groups": zapi.hostgroup.get(filter={"name": "Templates/Applications"}, output=['id']),
+        "tags": [
+            {
+                "tag": "etcd docker test",
+                "value": ""
+            }
+        ],
+        "templates": [
+            zapi.template.get(filter={"name": "Template App Etcd Cluster by HTTP"}, output=['id'])[0]
+        ],
+        "inventory_mode": 0
+    }
+
+    etcd_v32_1 = {
+        "host": "etcd_v32 node 1",
+        "interfaces": [
+            {
+                "type": 1,
+                "main": 1,
+                "useip": 0,
+                "ip": "",
+                "dns": "etcd32_1",
+                "port": "10050"
+            }
+        ],
+        "groups": zapi.hostgroup.get(filter={"name": "Templates/Applications"}, output=['id']),
+        "tags": [
+            {
+                "tag": "etcd docker test",
+                "value": ""
+            }
+        ],
+        "templates": [
+            zapi.template.get(filter={"name": "Template App Etcd Cluster by HTTP"}, output=['id'])[0]
+        ],
+        "inventory_mode": 0
+    }
+
+
+    etcd_v32_2 = {
+        "host": "etcd_v32 node 2",
+        "interfaces": [
+            {
+                "type": 1,
+                "main": 1,
+                "useip": 0,
+                "ip": "",
+                "dns": "etcd32_2",
+                "port": "10050"
+            }
+        ],
+        "groups": zapi.hostgroup.get(filter={"name": "Templates/Applications"}, output=['id']),
+        "tags": [
+            {
+                "tag": "etcd docker test",
+                "value": ""
+            }
+        ],
+        "templates": [
+            zapi.template.get(filter={"name": "Template App Etcd Cluster by HTTP"}, output=['id'])[0]
+        ],
+        "inventory_mode": 0
+    }
+
+    etcd_v32_3 = {
+        "host": "etcd_v32 node 3",
+        "interfaces": [
+            {
+                "type": 1,
+                "main": 1,
+                "useip": 0,
+                "ip": "",
+                "dns": "etcd32_3",
+                "port": "10050"
+            }
+        ],
+        "groups": zapi.hostgroup.get(filter={"name": "Templates/Applications"}, output=['id']),
+        "tags": [
+            {
+                "tag": "etcd docker test",
+                "value": ""
+            }
+        ],
+        "templates": [
+            zapi.template.get(filter={"name": "Template App Etcd Cluster by HTTP"}, output=['id'])[0]
+        ],
+        "inventory_mode": 0
+    }
+
+    zapi.host.create(etcd_v32_1)
+    zapi.host.create(etcd_v32_2)
+    zapi.host.create(etcd_v32_3)
+
+    zapi.host.create(etcd_v34_1)
+    zapi.host.create(etcd_v34_2)
+    zapi.host.create(etcd_v34_3)
