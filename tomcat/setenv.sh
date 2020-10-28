247555bfb0f52c74b2c7b0c29de3f5eeb16b0b80
Tikhon Uskov <tikhon.uskov@zabbix.com>
2020-10-28 16:47:28 +0300

    added tomcat

---
+++ tomcat/setenv.sh
@@ -0,0 +1,1 @@
+CATALINA_OPTS="-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=9000 -Dcom.sun.management.jmxremote.rmi.port=9000 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Djava.rmi.server.hostname=localhost -Dcom.sun.management.jmxremote.local.only=false"
